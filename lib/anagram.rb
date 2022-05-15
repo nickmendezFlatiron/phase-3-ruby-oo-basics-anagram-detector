fdca6f32a500f826463dc9545bb66285148a042a
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-05-15 17:30:42 -0400

    commit tree 39c5b9e6c9d47b2bd4bf48b511529eae9166a8b3

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
+require 'pry'
 # Your code goes here!
+
+class Anagram
+  attr_reader :word
+  attr_reader :arr
+  
+    def initialize(word)
+      @word = word
+    end
+    
+    def match(arr)
+      @arr = arr
+      result = arr.filter {|ana_word| ana_word == /(@word){#{@word.length}}/}      
+    end
+    
+    
+end
